c9abad661ed35165f221d01a8f63551ad808fba3
Zakai <Zakai@ZakaiBook.local>
2016-11-08 15:48:39 -0800

    add capybara and rack access to project

--- sweety/spec/spec_helper.rb
+++ sweety/spec/spec_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
+require "rack_session_access/capybara"
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
